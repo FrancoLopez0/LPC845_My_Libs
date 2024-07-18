325b7f1ac9b6894d57576681c4bb7618bc00770b
FrancoLopez0 <francoalelopez@gmail.com>
2024-07-18 14:51:53 -0300

    COMPASS

--- _I2C_/_I2C_.c
+++ _I2C_/_I2C_.c
@@ -5,12 +5,15 @@
  *      Author: Franco
  */
 
+
 #ifndef I2C__C_
 #define I2C__C_
 #include "_I2C_.h"
 
 int32_t rxBuff[14];
-hmc5883l_t magnetometer;
+//hmc5883l_t magnetometer;
+
+
 
 void I2C1_INIT_WRITE(uint8_t address){
 	while ((I2C1->STAT & 0x1) == 0); //WAIT STATE PENDING
@@ -57,15 +60,15 @@ void mpu6050(uint8_t* tx, uint8_t buff_size){
 
 	I2C1_STOP();
 }
-
-void hmc5883l(uint8_t* tx, uint8_t buff_size){
-	I2C1_INIT_WRITE(HMC5883L);
-
-	for(int i=0; i<buff_size; i++)I2C1_SEND_CONTINUE(tx[i]);
-
-
-	I2C1_STOP();
-}
+////
+//void hmc5883l(uint8_t* tx, uint8_t buff_size){
+//	I2C1_INIT_WRITE(HMC5883L);
+//
+//	for(int i=0; i<buff_size; i++)I2C1_SEND_CONTINUE(tx[i]);
+//
+//
+//	I2C1_STOP();
+//}
 
 void mpu6050_init(void){
 
@@ -97,79 +100,80 @@ void mpu6050_init(void){
 	mpu6050(txBuff,2); //ENABLE FIFO ACC
 }
 
-void hmc5883l_init(void){
-	uint8_t txBuff[2] = {0,0};
-
-//	txBuff[0] = 0x02;
-//	txBuff[1] = 0x00; // continuous measurement mode
-//	hmc5884l_init(txBuff, 2);
-
-	txBuff[0] = 0xA;
-	txBuff[1] = 0x80; // SET/RESET
-	hmc5883l(txBuff, 2);
-
-	txBuff[0] = 0x0B;
-	txBuff[1] = 0x1; // SET/RESET
-	hmc5883l(txBuff, 2);
-
-	txBuff[0] = 0x09; //Config
-	txBuff[1] = 0x1 | (0x2<<2) | (0x01<<4) | (0x0<<6); // continuous measurement mode
-//	txBuff[1] = 0x1 | (0x2<<3) | (0x01<<4) | (0x0<<6); // continuous measurement mode
-//	txBuff[1] = 0x1D;
-	hmc5883l(txBuff, 2);
-
-//	txBuff[0] = 0x00;
-//	txBuff[1] = 0x70; // continuous measurement mode
+//void hmc5883l_init(void){
+//	uint8_t txBuff[2] = {0,0};
+//
+////	txBuff[0] = 0x02;
+////	txBuff[1] = 0x00; // continuous measurement mode
+////	hmc5884l_init(txBuff, 2);
+//
+//	txBuff[0] = 0xA;
+//	txBuff[1] = 0x80; // SET/RESET
 //	hmc5883l(txBuff, 2);
 //
-//	txBuff[0] = 0x01;
-//	txBuff[1] = 0xA0; // continuous measurement mode
+//	txBuff[0] = 0x0B;
+//	txBuff[1] = 0x1; // SET/RESET
 //	hmc5883l(txBuff, 2);
 //
-//	txBuff[0] = 0x02;
-//	txBuff[1] = 0x00; // continuous measurement mode
+//	txBuff[0] = 0x09; //Config
+//	txBuff[1] = 0x1 | (0x2<<2) | (0x01<<4) | (0x0<<6); // continuous measurement mode
+////	txBuff[1] = 0x1 | (0x2<<3) | (0x01<<4) | (0x0<<6); // continuous measurement mode
+////	txBuff[1] = 0x1D;
 //	hmc5883l(txBuff, 2);
-}
+//
+////	txBuff[0] = 0x00;
+////	txBuff[1] = 0x70; // continuous measurement mode
+////	hmc5883l(txBuff, 2);
+////
+////	txBuff[0] = 0x01;
+////	txBuff[1] = 0xA0; // continuous measurement mode
+////	hmc5883l(txBuff, 2);
+////
+////	txBuff[0] = 0x02;
+////	txBuff[1] = 0x00; // continuous measurement mode
+////	hmc5883l(txBuff, 2);
+//}
 
-void hmc5883l_update(int32_t *rxBuff){
-	uint8_t txBuff[6] = {0x00, 0x1, 0x2, 0x3, 0x4, 0x5};
-	for(int i = 0; i < 6; i++)
-		{
+//
+//void hmc5883l_update(int32_t *rxBuff){
+//	uint8_t txBuff[6] = {0x00, 0x1, 0x2, 0x3, 0x4, 0x5};
+//	for(int i = 0; i < 6; i++)
+//		{
+//
+//		I2C1_INIT_WRITE(HMC5883L);
+//
+//		hmc5883l(&txBuff[i],1);
+//
+//		I2C1_INIT_READ(HMC5883L);
+//
+//		I2C1_RECEIVE_CONTINUE(&rxBuff[i], 1);
+//
+//		I2C1_STOP();
+//
+//		}
+//
+//}
 
-		I2C1_INIT_WRITE(HMC5883L);
-
-		hmc5883l(&txBuff[i],1);
-
-		I2C1_INIT_READ(HMC5883L);
-
-		I2C1_RECEIVE_CONTINUE(&rxBuff[i], 1);
-
-		I2C1_STOP();
-
-		}
-
-}
-
-void hmc5883l_get(hmc5883l_t* sense){
-	int rxBuff[6];
-
-	hmc5883l_update(rxBuff);
-
-	sense->mx = (int16_t)(rxBuff[0] | rxBuff[1] << 8);
-	sense->my= (int16_t)(rxBuff[2] | rxBuff[3] << 8);
-	sense->mz= (int16_t)(rxBuff[4] | rxBuff[5] << 8);
-
-}
-
-void hmc5883l_azimuth(hmc5883l_t* sense){
-	sense->azimuth= atan2((float)sense->my,(float)sense->mz) * 180.0 / PI;
-	sense->azimuth+=sense->magnetic_declination;
-		//sense->set_angle_objective -=
-	if(sense->azimuth<0){
-		sense->azimuth +=360;
-	}
-	sense->delta_angle = sense->set_angle_objective - sense->azimuth ; //Angular distance at desired orientation
-}
+//void hmc5883l_get(hmc5883l_t* sense){
+//	int rxBuff[6];
+//
+//	hmc5883l_update(rxBuff);
+//
+//	sense->mx = (int16_t)(rxBuff[0] | rxBuff[1] << 8);
+//	sense->my= (int16_t)(rxBuff[2] | rxBuff[3] << 8);
+//	sense->mz= (int16_t)(rxBuff[4] | rxBuff[5] << 8);
+//
+//}
+//
+//void hmc5883l_azimuth(hmc5883l_t* sense){
+//	sense->azimuth= atan2((float)sense->my,(float)sense->mz) * 180.0 / PI;
+//	sense->azimuth+=sense->magnetic_declination;
+//		//sense->set_angle_objective -=
+//	if(sense->azimuth<0){
+//		sense->azimuth +=360;
+//	}
+//	sense->delta_angle = sense->set_angle_objective - sense->azimuth ; //Angular distance at desired orientation
+//}
 
 void mpu6050_update(int32_t *rxBuff, uint8_t buff_size){
 	uint8_t txBuff[6] = {0x3B, 0x3C, 0x3D, 0x3E, 0x3F, 0x40};
--- _I2C_/_I2C_.h
+++ _I2C_/_I2C_.h
@@ -2,8 +2,15 @@
  * _I2C_.h
  *
  *  Created on: 6 jul. 2024
- *      Author: Franco
+ *      Author: Franco A. López
  */
+
+
+/**
+ * @file    _I2C_.h
+ * @brief   I2C communication.
+ */
+
 #define I2C_WAIT 10 //ms
 #define I2C_MASTER_CLK 100000
 
@@ -23,55 +30,105 @@
 #include "DELAY.h"
 #include "fsl_pint.h"
 #include "fsl_gpio.h"
+//#include "COMPASS.h"
 extern int32_t rxBuff[14];
 
-typedef struct{
-	int mx;
-	int my;
-	int mz;
-	float azimuth;
-	float magnetic_declination;
-	int set_angle_objective;
-	int delta_angle;
-}hmc5883l_t;
+//typedef struct{
+//	int mx;
+//	int my;
+//	int mz;
+//	float azimuth;
+//	float magnetic_declination;
+//	int set_angle_objective;
+//	int delta_angle;
+//}hmc5883l_t;
 
-extern hmc5883l_t magnetometer;
-typedef struct{
-	float aX;
-	float aY;
-	float aZ;
-}mpu6050_t;
+//extern hmc5883l_t magnetometer;
 
+//typedef struct{
+//	float aX;
+//	float aY;
+//	float aZ;
+//}mpu6050_t;
 
+//typedef struct{
+//	float aX;
+//	float aY;
+//	float aZ;
+//}mpu6050_t;
 
+/**
+ * @brief Init I2C output 1 as a master for write
+ *
+ * @param address I2C Address slave
+ *
+ * @return void
+ */
 void I2C1_INIT_WRITE(uint8_t);
 
+/**
+ * @brief Init I2C output 1 as a master for read
+ *
+ * @param address I2C Address slave
+ *
+ * @return void
+ */
 void I2C1_INIT_READ(uint8_t);
 
+/**
+ * @brief Send data
+ *
+ * @param data
+ *
+ * @return void
+ */
 void I2C1_SEND_CONTINUE(uint8_t);
 
+/**
+ * @brief Receive data
+ *
+ * @param rx Pointer to a buffer receive stack
+ * @param buff_size Buffer size
+ *
+ * @return void
+ */
 void I2C1_RECEIVE_CONTINUE(int32_t *, uint8_t);
 
+/**
+ * @brief Stop communication
+ *
+ * @param void
+ *
+ * @return void
+ */
 void I2C1_STOP(void);
+
+/**
+ * @brief Init I2C1 and select pins SDA, SCL
+ *
+ * @param void
+ *
+ * @return void
+ */
+void I2C1_INIT(void);
 
 void mpu6050(uint8_t*, uint8_t);
 
-void hmc5883l(uint8_t*, uint8_t);
+//void hmc5883l(uint8_t*, uint8_t);
 
 void mpu6050_init(void);
 
-void hmc5883l_init(void);
+//void hmc5883l_init(void);
 
-void hmc5883l_azimuth(hmc5883l_t*);
+//void hmc5883l_azimuth(hmc5883l_t*);
 
-void hmc5883l_update(int32_t *);
+//void hmc5883l_update(int32_t *);
 
 void SET_ANGLE_OBJECTIVE(pint_pin_int_t, uint32_t);
 
-void hmc5883l_get(hmc5883l_t*);
+//void hmc5883l_get(hmc5883l_t*);
 
 void mpu6050_update(int32_t *, uint8_t );
 
-void I2C1_INIT(void);
 
 void _printf_var_int(char*, int);
